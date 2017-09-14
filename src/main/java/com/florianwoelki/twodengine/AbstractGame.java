f89dd9550d0daf287e88510c3bdc4b2ab09429f3
sgtflow <sgtflow.yt@gmail.com>
2017-09-14 10:55:22 +0200

    Add JavaDoc
    There is now documentation to the following classes:
    - AbstractGame
    - Window

--- src/main/java/com/florianwoelki/twodengine/AbstractGame.java
+++ src/main/java/com/florianwoelki/twodengine/AbstractGame.java
@@ -7,29 +7,74 @@ import java.util.Stack;
 
 /**
  * Created by Florian Woelki on 18.11.16.
+ *
+ * This class represents an game and it delivers all
+ * the neccessary methods you need for creating a game.
+ * This will be your main game class and you can set
+ * here a state or update, render objects in your game.
  */
 public abstract class AbstractGame {
 
     private Stack<State> states = new Stack<>();
 
+    /**
+     * This method updates the game logic.
+     * It could be player movement or even
+     * physics.
+     *
+     * @param gc GameContainer for controlling the game
+     * @param dt Float delta for good fps on every computer
+     */
     public abstract void update(GameContainer gc, float dt);
 
+    /**
+     * This method renders all graphics and
+     * obstacles in the game.
+     * You can render rectangles, but also simple
+     * images.
+     *
+     * @param gc GameContainer for controlling the game
+     * @param renderer Renderer for rendering images or obstacles more easier
+     */
     public abstract void render(GameContainer gc, Renderer renderer);
 
-    public void push(State s) {
-        states.push(s);
+    /**
+     * This method will push a specific state to the current
+     * top list of all states. This will be your active state.
+     *
+     * @param state State for pushing to the top state
+     */
+    public void push(State state) {
+        states.push(state);
     }
 
+    /**
+     * This method will pop the current state and dispose everything
+     * from the state.
+     */
     public void pop() {
         states.peek().dispose();
         states.pop();
     }
 
-    public void setState(State s) {
+    /**
+     * With this method you set a state to a specific
+     * state of your choice.
+     * First it will pop the current state and then
+     * push the new specific state.
+     *
+     * @param state State for setting the state
+     */
+    public void setState(State state) {
         pop();
-        push(s);
+        push(state);
     }
 
+    /**
+     * With this method you peek the current state.
+     *
+     * @return State which was peeked
+     */
     public State peek() {
         return states.peek();
     }
--- src/main/java/com/florianwoelki/twodengine/window/Window.java
+++ src/main/java/com/florianwoelki/twodengine/window/Window.java
@@ -10,6 +10,11 @@ import java.awt.image.BufferedImage;
 
 /**
  * Created by Florian Woelki on 21.11.16.
+ *
+ * This class represents a simple Window creating
+ * with swing.
+ * It contains also setting up the rendering stuff
+ * for the window.
  */
 public class Window {
 
@@ -22,15 +27,23 @@ public class Window {
     private Graphics g;
     private BufferStrategy bs;
 
+    /**
+     * This constructor will setup the window and the
+     * rendering part for the window.
+     *
+     * @param gc GameContainer for getting initial values
+     */
     public Window(GameContainer gc) {
         image = new BufferedImage(gc.getWidth(), gc.getHeight(), BufferedImage.TYPE_INT_RGB);
 
+        // Creating the size of the window
         canvas = new Canvas();
         Dimension size = new Dimension((int) (gc.getWidth() * gc.getScale()), (int) (gc.getHeight() * gc.getScale()));
         canvas.setPreferredSize(size);
         canvas.setMaximumSize(size);
         canvas.setMinimumSize(size);
 
+        // Creating the frame.
         frame = new JFrame(gc.getTitle());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(canvas);
@@ -39,16 +52,25 @@ public class Window {
         frame.setResizable(false);
         frame.setVisible(true);
 
+        // Creating the rendering part
         canvas.createBufferStrategy(1);
         bs = canvas.getBufferStrategy();
         g = bs.getDrawGraphics();
     }
 
+    /**
+     * This method will update the window and it will
+     * draw the BufferedImage again.
+     */
     public void update() {
         g.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
         bs.show();
     }
 
+    /**
+     * This method cleans up the window and it
+     * dispose everything.
+     */
     public void cleanUp() {
         g.dispose();
         bs.dispose();
